e213528d75fe957b10f861d757a14b6141a40f09
mimio2009 <orckide.234@gmail.com>
2017-07-21 14:18:23 +0000

    add delete action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -41,6 +41,15 @@ class TodosController < ApplicationController
      
    end
    
+   def destroy
+     
+     @todo = Todo.find(params[:id])
+     @todo.destroy 
+     flash[:notice] = "Todo was deleted successfully"
+     redirect_to todos_path
+     
+   end
+   
   private
   
   def todo_params
